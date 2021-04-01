d42ff834d701604c21a386801cc8b11197cb871a
anilk2hms <anilkumar.degala@childrens.harvard.edu>
2021-04-01 11:32:49 -0400

    fixes for download key handling

--- airflow-data/scripts/download_s3_file.sh
+++ airflow-data/scripts/download_s3_file.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
   
 echo "Download S3 File begin"
-echo $1 # s3_bucket
-echo $2 # log_file
-echo $3 # s3_object
-echo $4 # skip 
+echo $1 # download_key
+echo $2 # file
+echo $3 # skip 
 
-if [[ "$4" == "N" ]]; then
+if [[ "$3" == "N" ]]; then
 	_user="$(id -u -n)"
 	_uid="$(id -u)"
 	ls -altr 
 	cd /opt/bitnami/airflow/airflow-data/downloads
-	aws s3 cp  s3://$1/$2".encrypted" $3".encrypted" 
+	aws s3 cp  s3://$1".encrypted" $2".encrypted" 
 	chmod -R 777 /opt/bitnami/airflow/airflow-data/downloads
 	ls -altr
 elif [[ "$3" == "Y" ]]; then
